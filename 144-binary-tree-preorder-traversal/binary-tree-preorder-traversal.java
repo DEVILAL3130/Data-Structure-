4d70043c9fbf90991feb6839d81c98857e24f1b4
Devilal Gupta <143402714+DEVILAL3130@users.noreply.github.com>
2026-04-15 05:02:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (96.94%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -13,19 +13,27 @@
  *     }
  * }
  */
-
- class Solution {
+class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
-        preorder(root, result);
+        
+        if (root == null) return result;
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            result.add(node.val);
+
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+
         return result;
     }
-
-    private void preorder(TreeNode root, List<Integer> result) {
-        if (root == null) return;
-
-        result.add(root.val);        
-        preorder(root.left, result); 
-        preorder(root.right, result);
-    }
-}      
+}
